9accb7b3d3ebabae4d197319c99e7ddd58f4d6f4
kanan-git <kanan.bakhshaliyev.1997@gmail.com>
2026-01-22 18:17:10 +0400

    .

--- CLASSROOM NOTES/2026 01 13/task2/obj/Debug/net10.0/task2.AssemblyInfo.cs
+++ CLASSROOM NOTES/2026 01 13/task2/obj/Debug/net10.0/task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bef4c344db63c1018a2eb626ad91c9bdf1b40c3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5778dff690d5c20a6a5d86d54bc98287c5e278e")]
 [assembly: System.Reflection.AssemblyProductAttribute("task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CLASSROOM NOTES/2026 01 16/Task1/Models/obj/Debug/net10.0/models.AssemblyInfo.cs
+++ CLASSROOM NOTES/2026 01 16/Task1/Models/obj/Debug/net10.0/models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bef4c344db63c1018a2eb626ad91c9bdf1b40c3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5778dff690d5c20a6a5d86d54bc98287c5e278e")]
 [assembly: System.Reflection.AssemblyProductAttribute("models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
