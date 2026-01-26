c07cfc40652a321c957ab6ffd84574a70a34163d
kanan-git <kanan.bakhshaliyev.1997@gmail.com>
2026-01-26 16:31:46 +0400

    MS SQL 1st lesson

--- CLASSROOM NOTES/2026 01 13/task2/obj/Debug/net10.0/task2.AssemblyInfo.cs
+++ CLASSROOM NOTES/2026 01 13/task2/obj/Debug/net10.0/task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6efcf2be43dbc72929fde5abde1815d61a500d40")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38d67d6cfbfc602b12972b145582a61ede21b736")]
 [assembly: System.Reflection.AssemblyProductAttribute("task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
